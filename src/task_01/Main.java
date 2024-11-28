b254def90c5ea3208af9a6407140a10e613b5a3c
SerhiiOladko <serolad84@gmail.com>
2024-11-28 21:48:49 +0100

    Update Main.java

--- src/task_01/Main.java
+++ src/task_01/Main.java
@@ -1,19 +1,1 @@
-package task_01;
 
-class SingleThreadSolution {
-
-    public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-
-        int count = 0;
-        for (int i = 1; i <= 2_000_000; i++) {
-            if (i % 21 == 0 && String.valueOf(i).contains("3")) {
-                count++;
-            }
-        }
-
-        long endTime = System.currentTimeMillis();
-        System.out.println("Результат в один поток: " + count);
-        System.out.println("Время выполнения: " + (endTime - startTime) + " мс");
-    }
-}
